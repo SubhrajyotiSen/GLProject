497326ab2b28e574bda9eff11bb94d8010ef0df2
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-04-19 22:34:38 +0530

    Now Earth even revolves

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@
 
 using namespace std;
 
+#define DEG2RAD 3.14/180
+
 const float size = 10.0f;
 #define M_PI	(3.14159265)
 GLUquadricObj *sphere = NULL;
 
 float angle = 0;
 GLuint _textureId;
+
+int counterForEarth = 0;
 
 void initRendering() {
 	glEnable(GL_DEPTH_TEST);
@@ -39,15 +43,35 @@ void drawScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glTranslatef(0.0f, 1.0f, -16.0f);
+
+	// Revolution
+	glBegin(GL_POINTS); 
+	for (int i=0; i < 360; i++) 
+	{ 
+		float degInRad = i*DEG2RAD; 
+		glVertex3f(cos(degInRad)*6.0, sin(degInRad)*2.3,0.0f);
+	} 
+	glEnd(); 
+	if(counterForEarth>359) 
+	counterForEarth = 0;
+	else counterForEarth++;
+	float xForEarth = cos(counterForEarth*DEG2RAD)*6.0f;
+	float yForEarth = sin(counterForEarth*DEG2RAD)*2.3f;
+	glPushMatrix(); 
+	glTranslatef(xForEarth,yForEarth,-16.0f);
+	
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, _textureId);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	// Rotation
 	glRotatef(90,1.0f,0.0f,0.0f);
 	glRotatef(angle,0.0f,0.0f,1.0f);
 	gluQuadricTexture(sphere,1);
 	gluSphere(sphere,4,20,20);
+
+	// Swap buffers since double buffers are used
 	glutSwapBuffers();
 }
 
